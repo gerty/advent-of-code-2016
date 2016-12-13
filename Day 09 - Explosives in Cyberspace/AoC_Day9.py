8c95da0edfd5960ff5158b8fc028fc1f52530d6c
gerty <gerty33@gmail.com>
2016-12-12 23:16:56 -0600

    9-1 in work

--- Day 09 - Explosives in Cyberspace/AoC_Day9.py
+++ Day 09 - Explosives in Cyberspace/AoC_Day9.py
@@ -11,18 +11,24 @@ with open('Day9input.txt', 'r') as f:
     rawdata = f.readlines()
 
 def decompress(inputstring):
-    tempstring1 = ''
-    incopymode = False
-    ministring = ''
-    for ch in inputstring:
-        tempstring1 += ch
-    # parse the string until reaching a '('
-    # go into copy mode and read the numbers by re-splitting at the x and the ')'
-    # use the numbers to count and copy into ministring
-    # exit copymode and continue copying 1:1 and look for the next '('
-    # until we're done with the whole lot
-    # then,
-    return(tempstring1)
+    instring = inputstring  # <-- our input string, but in a local variable so we can do with it what we want
+    decompstring = ''       # <-- final decompressed string we build up as the answer
+    codes = ''             # <-- the tiny string of data inside the '()'
+    sniplength = 0         # <-- tracks the length of string to be snipped for copying
+    numcopies = 0          # <-- tracks the number of copies of the copystring needed
+    copystring = ''        # <-- the shorter string we load up for copying per the values in '()'
+    index = 0
+    while len(instring) > 0:
+        decompstring += instring.split('(')[0]  # simply transfer the string until reaching a '('
+        codes = instring.split('()')[1]  # should return us something like '3x2', '1x40', or '0x34' ** out of range **
+        sniplength = int(codes.split('x')[0])  # the length of the copystring is the first number
+        numcopies = int(codes.split('x')[1])   # the number of chars in the copystring is the second number
+        copystring = instring.split(')')[1][0:sniplength]  # create the copystring ** removing later-appearing ()s!! **
+        for i in range(numcopies):         # copy the sniplength-long copystring numcopies times
+            decompstring += copystring
+        instring = ''.join(instring.split(')')[1:])  # remove the codes from instring ** but not all of them!! **
+        instring = instring[len(copystring):]   # also take away the chars that made up the copystring
+    return(tempstring1)   # when instring is empty, return the answer
 
 uncompdata = decompress(rawdata[0])
 print(uncompdata)

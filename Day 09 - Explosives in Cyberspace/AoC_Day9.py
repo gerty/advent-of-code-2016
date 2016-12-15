94d820fec666ada2bc46b187c2c27290d9182ce0
gerty <gerty33@gmail.com>
2016-12-15 00:21:15 -0600

    Doesn't work - off by 4 for some reason after reorg of code to make simpler. Commit now is so I can trace difference.

--- Day 09 - Explosives in Cyberspace/AoC_Day9.py
+++ Day 09 - Explosives in Cyberspace/AoC_Day9.py
@@ -7,43 +7,46 @@
 inputchar = ''
 uncompdata = ''
 
-with open('Day9input.txt', 'r') as f:
-    rawdata = f.readlines()
 
 def decompress(inputstring):
-    instring = inputstring  # <-- our input string, but in a local variable so we can do with it what we want
+    instring = str(inputstring)  # <-- our input string, but in a local variable so we can do with it what we want
     decompstring = ''       # <-- final decompressed string we build up as the answer
 
     codes = ''             # <-- the tiny string of data inside the '()'
-    snip = 0         # <-- tracks the length of string to be snipped for copying
+    sniplen = 0         # <-- tracks the length of string to be snipped for copying
     copies = 0          # <-- tracks the number of copies of the copystring needed
     copystring = ''        # <-- the shorter string we load up for copying per the values in '()'
 
-    if '(' in instring:
-        decompstring += instring.split('(', 1)[0]  # copy chars exact until reaching a '(', then stop
-    if '(' in instring:
-        instring = instring.split('(', 1)[1]  # remove the easily transferred parts and reassign the rest to instring
-
-    while len(instring) > 0:                    # always start here with a code ready without '(', or empty string
-        codes = instring.split(')',1)[0]  # should return us something like '3x2', '1x40', or '0x34'
-        print(codes)
-        snip = int(codes.split('x')[0])  # the length of the copystring is the first number
-        copies = int(codes.split('x')[1])   # the number of chars in the copystring is the second number
-        copystring = instring.split(')',1)[1][0:snip]  # create the copystring
-        for i in range(copies):         # copy the sniplength-long copystring numcopies times
-            decompstring += copystring
-        instring = instring.split(')',1)[1]  # remove codes from instring
-        instring = instring[len(copystring):]   # also take away the chars that made up the copystring
+    while len(instring) > 0:
         if '(' in instring:
-            decompstring += instring.split('(', 1)[0]  # copy chars exact until reaching a '(', then stop
-        else:
-            decompstring += instring   # if there are no codes left, dump the rest and empty instring
+            decompstring += instring.split('(', 1)[0]  # simple copy of chars until reaching a '('
+            instring = instring.split('(', 1)[1]  # remove transferred parts and reassign the rest to instring
+
+        #  Now we either have a string of only characters, or the beginning of a tag to process
+
+        if ')' in instring:
+            codes = str(instring.split(')', 1)[0])  # should return us something like '3x2', '1x40', or '0x34'
+            print(codes)
+            sniplen = int(codes.split('x')[0])  # the length of the copystring is the first number
+            copies = int(codes.split('x')[1])   # the number of chars in the copystring is the second number
+            copystring = instring.split(')', 1)[1][0:sniplen]  # create the copystring
+            print(copystring)
+            for i in range(copies):         # copy the sniplength-long copystring numcopies times
+                decompstring += (copystring) # Could it be that I only need to change ONE LINE for part 2???
+            instring = instring.split(')', 1)[1]  # remove codes from instring
+            instring = instring[len(copystring):]   # also take away the chars that made up the copystring
+
+        # if there are no codes left, dump the rest and empty instring
+
+        if not ('(' in instring):
+            decompstring += instring
             instring = ''
-        if '(' in instring:
-            instring = instring.split('(', 1)[1]  # remove the easily transferred parts, reassign the rest to instring
-    return(decompstring)   # when instring is empty, return the answer
+    return decompstring   # when instring is empty, return the answer
 
-uncompdata = decompress(rawdata[0])
+with open('Day9input.txt', 'r') as f:
+    rawdata = f.readlines()
+
+uncompdata = decompress(rawdata)
 print(len(uncompdata))
 
 # My first, unrealistic-looking guess was 107036. Wrong and too high.

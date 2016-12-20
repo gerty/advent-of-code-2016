93e01d122a7e0e0cdc398edc353c00abf6850c0c
gerty <gerty33@gmail.com>
2016-12-20 01:16:26 -0600

    First version with realistic answer for Day 11.

--- Day 11 - Radioisotope Thermoelectric Generators/AoC_Day11.py
+++ Day 11 - Radioisotope Thermoelectric Generators/AoC_Day11.py
@@ -24,54 +24,55 @@ import random
 
 # Tracking variables:
 elevator = 1
-generators = [1, 2, 2, 2, 2]
-microchips = [1, 3, 3, 3, 3]
-genmove = [0, 0, 0, 0, 0]
-micmove = [0, 0, 0, 0, 0]
-totalmoves = 0
-success = False
-bestmoves = 1000000
+generators = [1, 2, 2, 2, 2]  # locations of each generator
+microchips = [1, 3, 3, 3, 3]  # locations of each microchip
+genmove = [0, 0, 0, 0, 0]     # movement of each generator
+micmove = [0, 0, 0, 0, 0]     # movement of each microchip
+totalmoves = 0              # tracks total moves in each try
+success = False             # looks for success criteria
+bestmoves = 1000000         # artificially high starting point
 
 # Checking functions:
 def legalstate(elev, gens, micros):
-    if (elev not in gens) and (elevator not in micros):  # elev functions if contains at least one RTG / chip
-        return False
     for x in range(5):
         if gens[x] != micros[x]:  # if generator is not with its microchip
             if micros[x] in gens:  # and if microchip floor contains any generators
                 return False                # then invalid due to frying of microchip
-    if (elevator not in micros) and (elevator not in gens):  # lonely elevator
-        return False
+    return True
 
 # Not the efficient or elegant solution, but given the highly constraining rules, I decided to have parts randomly
-# whether to get on a randomly moving elevator. I will cap the number of moves at the most successful solution, making
-# the number get smaller and smaller as it goes. Well, we'll see...
+# choose whether or not to get on a randomly moving elevator. I will cap the number of moves at the most successful
+# solution, making the number get smaller and smaller as it goes. Well, we'll see...
 while not success:
     genmove = [0, 0, 0, 0, 0]  # track movements of the generators
     micmove = [0, 0, 0, 0, 0]  # track movements of the microchips
-    elevdir = int(0)
     if elevator == 1:  # must go up
         elevdir = 1
     if elevator == 4:  # must go down
         elevdir = -1
     if elevator == 2 or elevator == 3:
         elevdir = random.choice([-1, 1])   # makes a random choice between up and down
+
     for i in range(5):
         if generators[i] == elevator:   # considering only generators that are on the same floor as an elevator
             genmove[i] = random.choice([0, elevdir])  # choose randomly whether to get on it or not
         if microchips[i] == elevator:   # considering only microchips that are on the same floor as an elevator
             micmove[i] = random.choice([0, elevdir])  # choose randomly whether to get on it or not
-    passengers = genmove.count(elevdir) + micmove.count(elevdir)
-    if 0 < passengers <= 2:  # if moving parts is between 1 and 2
-        if legalstate(elevator + elevdir, generators + genmove, microchips + micmove):  # legal future state test
+
+    if 1 <= genmove.count(elevdir) + micmove.count(elevdir) <= 2:  # if moving parts is between 1 and 2
+        if legalstate(elevator + elevdir,
+                      [x + y for x, y in zip(generators, genmove)],
+                      [xx + yy for xx, yy in zip(microchips, micmove)]):  # legal future state test
             elevator += elevdir
-            generators += genmove
-            microchips += micmove
+            generators = [x + y for x, y in zip(generators, genmove)]
+            microchips = [xx + yy for xx, yy in zip(microchips, micmove)]
             totalmoves += 1
+
     if generators == microchips == [4, 4, 4, 4, 4]:  # if all items are on 4th floor
         success = True
         print(totalmoves, "as a new minimum number of moves!")
         bestmoves = totalmoves
+
     if totalmoves >= bestmoves:  # if we have failed to beat our best score
         elevator = 1
         generators = [1, 2, 2, 2, 2]
